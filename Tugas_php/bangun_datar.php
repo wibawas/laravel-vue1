172225dc060ed41f900008addb38b39727ff2ecd
Hangga Wibawa Setya <hanggaaccnt@gmail.com>
2022-12-19 11:07:41 +0700

    Tugas PHP Operator Matematika

---
+++ Tugas_php/bangun_datar.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "----- bangun datar ----- <br>" ;
+
+$persegiPanjang = 5 * 7 ;
+echo "Luas Persegi = {$persegiPanjang} <br>" ;
+
+$segitiga = 1/2 * 4 *7 ;
+echo "Luas segitiga = {$segitiga} <br>" ;
+
+$persegi = 5**2 ;
+echo "Luas persegi = {$persegi} <br>" ;
+
+$trapesium = 1/2 *13*8 ;
+echo "Luas trapesium = {$trapesium} <br>" ;
+
+$layang_layang = 1/2*7*9 ;
+echo "Luas layang-layang = {$layang_layang} <br><br>";
---
+++ Tugas_php/bangun_ruang.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo "----- Bangun Ruang ----- <br>";
+
+$balok = 4*9*14;
+echo"Volume balok = {$balok} <br>" ;
+
+$kubus = 7**3 ;
+echo "Volume Kubus = {$kubus} <br>" ;
+
+$prisma = 1/2 *13*3*5;
+echo "Volume prisma = {$prisma} <br>";
